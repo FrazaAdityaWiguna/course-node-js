caa8d80c3e4ba2210de100c2fd6633f08300a499
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-11-28 23:58:57 +0700

    feat: learn writing files

--- files.js
+++ files.js
@@ -5,16 +5,28 @@ const fs = require("fs");
  * args 1 = file path
  * args 2 = cb
  */
-fs.readFile("./docs/blog1.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
-});
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
 
-console.log("last line");
+// console.log("last line");
 
 // writing files
+/**
+ * args 1 = file path
+ * args 2 = replace content
+ * args 3 = cb
+ */
+fs.writeFile("./docs/blog1.txt", "Hello, bigdream!", () =>
+  console.log("File was written")
+);
+
+fs.writeFile("./docs/blog2.txt", "Hello, pirates!", () =>
+  console.log("File was written")
+);
 
 // directories
 
