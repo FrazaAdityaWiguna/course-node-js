e78b5bb0f1d0667b004133227a7ea180257a7aa5
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-11-29 00:05:53 +0700

    feat: directories

--- files.js
+++ files.js
@@ -20,14 +20,32 @@ const fs = require("fs");
  * args 2 = replace content
  * args 3 = cb
  */
-fs.writeFile("./docs/blog1.txt", "Hello, bigdream!", () =>
-  console.log("File was written")
-);
+// fs.writeFile("./docs/blog1.txt", "Hello, bigdream!", () =>
+//   console.log("File was written")
+// );
 
-fs.writeFile("./docs/blog2.txt", "Hello, pirates!", () =>
-  console.log("File was written")
-);
+// fs.writeFile("./docs/blog2.txt", "Hello, pirates!", () =>
+//   console.log("File was written")
+// );
 
 // directories
+if (!fs.existsSync("./assets")) {
+  /**
+   * args 1 = file name
+   * args 2 = cb
+   */
+  fs.mkdir("./assets", () => {
+    console.log("file created");
+  });
+} else {
+  /**
+   * args 1 = file name
+   * args 2 = cb
+   */
+  fs.rmdir("./assets", (err) => {
+    if (err) console.log(err);
+    console.log("folder deleted");
+  });
+}
 
 // deleting files
