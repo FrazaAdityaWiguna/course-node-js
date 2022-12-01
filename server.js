66ad3611eaa4f902aa29e150af08649dedb2d180
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-01 10:53:25 +0700

    feat: returning html response

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
@@ -6,10 +7,15 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader("Content-Type", "text/html");
 
-  res.write('<head><link rel="stylesheet" href="#"></head>');
-  res.write("<h1>Hello, Fraza</h1>");
-  res.write("<h2>Hello, Nada</h2>");
-  res.end();
+  // send an html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.end();
+    }
+
+    res.end(data);
+  });
 });
 
 server.listen(3000, "localhost", () => {
