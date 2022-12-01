d34ae10b50ba6b8e5665974db1a029d71497f0d8
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-01 10:34:05 +0700

    feat: learn response #1

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request mode");
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write("<h1>Hello, Fraza</h1>");
+  res.write("<h2>Hello, Nada</h2>");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
