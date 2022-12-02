8a940f523d8ae0fece0b10b8dfdb4e4f6d85ff9f
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-02 22:55:47 +0700

    feat: ejs view engine

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// register view engine with ejs
+app.set("view engine", "ejs");
+
+// if folder views change name
+// app.set("views", "myViews");
+
 // listen for request
 app.listen(port);
 
@@ -11,11 +17,11 @@ app.get("/", (req, res) => {
   // res.send("<p>hello express</p>");
 
   // console.log("Dirname: ", __dirname);
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
 // redirect
@@ -23,7 +29,11 @@ app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
+});
+
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
