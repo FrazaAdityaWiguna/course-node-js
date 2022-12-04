8a159a709d171cff1862f7eeaf32de593ba365b8
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-04 21:37:29 +0700

    feat: get and saving data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express application
 const app = express();
@@ -8,11 +9,14 @@ const port = 3000;
 
 // connect to monggoDB
 const dbURI =
-  "mongodb+srv://admin:admin1234@nodetuts.ydef603.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://admin:admin1234@nodetuts.ydef603.mongodb.net/note-tuts?retryWrites=true&w=majority";
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((res) => app.listen(port))
+  .then((res) => {
+    console.log("Connect to MongoDB");
+    app.listen(port);
+  })
   .catch((err) => console.log("err: ", err));
 
 // register view engine with ejs
@@ -25,6 +29,40 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(morgan("dev"));
+
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+// get all data blogs
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+// get single blog
+app.get("/single-blog", (req, res) => {
+  Blog.findById("638caead270103a3e761cc17")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.get("/", (req, res) => {
   // res.send("<p>hello express</p>");
